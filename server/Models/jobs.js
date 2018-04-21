cf4a27a67a4ed068773938bdcb622364bc5f4923
HamzaAlwan <ihamzaalwan@gmail.com>
2018-04-21 15:47:08 +0300

    created job schema

--- server/Models/jobs.js
+++ server/Models/jobs.js
@@ -1,18 +1,84 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-////Gobs Schema
-var jobsSchema=mongoose.Schema({
-  user:{type:String},
-  jobTitle:String,
-  jobDescription:String,
-  category:String,
-  startTime:String,
-  endTime:String,
-  created_at:{
+
+////Jobs Schema
+var jobsSchema = mongoose.Schema({
+  user: 
+  {
+  	type: String,
+  },
+  jobTitle: {
+  	type: String,
+  	require:true
+  },
+  jobDescription: String,
+  category: {
+  	type: String,
+  	require:true
+  },
+  from: Number,
+  to: Number,
+  created_at: 
+  {
     type:Date,
     default:Date.now
   }
+});
 
-})
+
 /////Jobs Model
 var Jobs = mongoose.model('Jobs', jobsSchema);
+
+var createJob = function(data, callback){
+	Jobs.create(data, callback)
+};
+
+var allJobs = function (data, callback){
+	Jobs.find({}, function(err, data){
+		if (err){
+			throw err
+		}
+		callback(data)
+	});
+};
+
+var jobsByCatagory = function(category, callback){
+	Jobs.findOne({category: category}, function(err, data){
+    	if(err){
+			throw err
+		}
+		callback(data)
+	});
+};
+
+var jobsByStartTime = function(from, callback){
+	Jobs.find({from: from}, function(err, data){
+		if(err){
+			throw err
+		}
+		callback(data)
+	});
+};
+
+var jobsByEndTime = function(to, callback){
+	Jobs.find({to: to}, function(err, data){
+		if(err){
+			throw err
+		}
+		callback(data)
+	});
+};
+
+var updateJob = function(jobTitle, updatedData, callback){
+	Jobs.findOneAndUpdate({jobTitle: jobTitle},  { $set: updatedData}, callback)
+};
+
+
+// Exporting the Model and the functions
+module.exports.Jobs = Jobs;
+module.exports.createJob = createJob;
+module.exports.allJobs = allJobs;
+module.exports.jobsByCatagory = jobsByCatagory;
+module.exports.jobsByStartTime = jobsByStartTime;
+module.exports.jobsByEndTime = jobsByEndTime;
+module.exports.updateJob = updateJob;
