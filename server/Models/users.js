73d418fe2dd3d59d99b83ba5d59819ae18631c70
HamzaAlwan <ihamzaalwan@gmail.com>
2018-04-21 20:35:10 +0300

    finished the post/get req for jobs exept for (time) and some other fixes

--- server/Models/users.js
+++ server/Models/users.js
@@ -3,57 +3,64 @@ mongoose.Promise = global.Promise;
 var bcrypt = require('bcrypt-nodejs');
 
 ////User Schema
-
-
 var usersSchema = mongoose.Schema({
   userName: {
     type:String,
     unique:true,
     trim: true
   },
-  name:String,
+  name:{
+        type: String,
+        required: true
+      },
   password: String,
   gender:String,
-  phoneNumber:Number,
+  phoneNumber:{
+        type: Number,
+        required: true
+      },
   Address:String,
-  Age:Number,
+  Age:{
+        type: Number,
+        required: true
+      },
   Nationality:String
-
 });
+
 //User Model
 var Users = mongoose.model('Users', usersSchema);
 
-var comparePassword= function(attemptedPassword, callback) {
+var comparePassword = function(attemptedPassword, callback) {
    callback(bcrypt.compareSync(attemptedPassword, hash));
-  }
-var hashPassword= function(password,callback) {
+  };
+var hashPassword = function(password, callback) {
   const saltRounds = 10;
   var salt = bcrypt.genSaltSync(saltRounds);
   var hash = bcrypt.hashSync(password, salt);
-  callback(hash)
-  }
+  callback(hash);
+  };
 
-
-var createUsers=function(data,callback){
-  var userdata=data;
-  hashPassword(data.password,function(hashed){
-    userdata["password"]=hashed;
-  })
-  
-  Users.create(userdata,callback);
-}
-var getUser=function(userName,callback){
+var createUsers = function(data, callback){
+  var userdata = data;
+  hashPassword(data.password, function(hashed){
+    userdata["password"] = hashed;
+  });
+  Users.create(userdata, callback);
+};
+var getUser = function(userName, callback){
   var query  = Users.where({ userName: userName });
   query.findOne(callback);
-}
-var updateUsers=function(userName,updatedData,callback){
-  Users.findOneAndUpdate({userName:userName},  { $set: updatedData}, callback)
-}
-var deleteUser=function(userName,callback){
-  Users.deleteOne({userName:userName}, callback)
-}
-module.exports.Users=Users
-module.exports.createUsers=createUsers
-module.exports.updateUsers=updateUsers
-module.exports.deleteUser=deleteUser
-module.exports.getUser=getUser
+};
+var updateUsers = function(userName, updatedData, callback){
+  Users.findOneAndUpdate({userName: userName}, {$set: updatedData}, callback);
+};
+var deleteUser = function(userName, callback){
+  Users.deleteOne({userName: userName}, callback);
+};
+
+// Exporting the Model and the functions
+module.exports.Users = Users;
+module.exports.createUsers = createUsers;
+module.exports.getUser = getUser;
+module.exports.updateUsers = updateUsers;
+module.exports.deleteUser = deleteUser;
