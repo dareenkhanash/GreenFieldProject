11dd36002548f3e3ae0e329e26b4f62b7a864eb3
Atheer83 <atheer.for.tech@gmail.com>
2018-04-23 11:45:21 +0300

    add routes to the project

---
+++ react-client/src/routes/AppRouter.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
+import SignUpForm from '../components/SignUp.jsx';
+import JobsForm from '../components/JobsForm.jsx';
+import Header from '../components/Header.jsx';
+import NotFoundPage from '../components/NotFoundPage.jsx';
+
+const AppRouter = () => (
+	<BrowserRouter>
+		<div>
+			<Header />
+			<Switch>
+			<Route path = "/" exact = {true} />	
+			<Route path = "/signup" component = {SignUpForm} />				
+			<Route path = "/jobsForm" component = {JobsForm} />				
+			<Route component = {NotFoundPage} />				
+
+			</Switch>
+		</div>
+	</BrowserRouter>
+
+	)
+
+export default AppRouter;
