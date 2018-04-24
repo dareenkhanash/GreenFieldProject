43426245780ace24e6c0257362b0334a056f21a1
HamzaAlwan <ihamzaalwan@gmail.com>
2018-04-24 17:19:04 +0300

    style

--- react-client/src/routes/AppRouter.jsx
+++ react-client/src/routes/AppRouter.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
 import SignUpForm from '../components/SignUp.jsx';
 import JobsForm from '../components/JobsForm.jsx';
-import Header from '../components/Header.jsx';
+import NavBar from '../components/Header.jsx';
 import NotFoundPage from '../components/NotFoundPage.jsx';
 import Login from '../components/Login.jsx';
 import Home from '../components/Home.jsx';
 const AppRouter = () => (
 	<BrowserRouter>
 		<div>
-			<Header />
+			<NavBar /><br /><br /><br />
 			<Switch>
 			<Route path = "/" exact = {true} component = {Home}/>	
 			<Route path = "/signup" component = {SignUpForm} />				
