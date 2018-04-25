8f6651e7b186240c8b52c08d77a83b2387cd05f5
Elena-Livia <elena.livia@yahoo.com>
2018-04-25 16:45:06 +0300

    jobs rendering

--- react-client/src/components/HomeDisplay.jsx
+++ react-client/src/components/HomeDisplay.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Button, FormControl, Row, Col, ButtonToolbar } from 'react-bootstrap';
 class HomeDisplay extends React.Component {
   constructor(props) {
     super(props);
@@ -11,7 +11,51 @@ class HomeDisplay extends React.Component {
 render() {
   
   return (
-    <h1>{this.props.item.jobTitle}</h1>)
+  	<div>
+  	<div className="jobsDiv">
+  		<Row>
+			
+			<Col md={6}>
+			<span><b>Job Title : </b></span>
+			<span>{this.props.item.jobTitle}</span>
+			</Col>
+			<Col md={6}>
+			<span><b>Job Category : </b></span>
+			<span>{this.props.item.category}</span>
+			</Col>
+			
+			
+		</Row>
+		<Row>
+			
+
+			<Col md={12}>
+			<span><b>Description : </b></span>
+			<span>{this.props.item.jobDescription}</span>
+			</Col>
+			
+		</Row>
+        <Row>
+			
+            <Col md={4}>
+            <span><b>From : </b></span>
+			<span>{this.props.item.from}</span>
+			</Col>
+			<Col md={4}>
+			<span><b>To : </b></span>
+			<span>{this.props.item.to}</span>
+			</Col>
+			<Col md={4}>
+			<span><b>Created at : </b></span>
+			<span>{this.props.item.created_at}</span>
+			</Col>
+		</Row>
+    
+    
+    
+    
+    </div>
+    </div>)
   }
 }
 export default HomeDisplay;
