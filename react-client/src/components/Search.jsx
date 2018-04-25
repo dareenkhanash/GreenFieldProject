4862454f68e1867c4a19fe0cfbe0889f0ecd7de5
HamzaAlwan <ihamzaalwan@gmail.com>
2018-04-25 14:19:39 +0300

    add select

--- react-client/src/components/Search.jsx
+++ react-client/src/components/Search.jsx
@@ -19,22 +19,29 @@ class Search extends React.Component {
 
   render() {
     return (
-      <div className="search-bar form-inline">
-        <DropdownButton title="Catagory" id="catB">
-      <MenuItem eventKey="1">Driver</MenuItem>
-      <MenuItem eventKey="2">Maintenance</MenuItem>
-      <MenuItem eventKey="3">Job3</MenuItem>
-      <MenuItem eventKey="4">Job4</MenuItem>
-      </DropdownButton>
-        <FormControl
+      <div className="search-bar form-inline">  
+
+      <div className="form-group">
+        <select className="form-control selectpicker btn btn-default" id="catB">
+          <option value="select">Select Catagory</option>
+          <option value="Driver">Driver</option>
+          <option value="Home Maintenance">Home Maintenance</option>
+          <option value="Computer Maintenance">Computer Maintenance</option>
+          <option value="Babysitting">Babysitting</option>
+          <option value="Tutoring">Tutoring</option>
+          <option value="Others">Others</option>
+        </select>
+        </div>
+
+      <FormControl
           className="Sform-control"
           type="text"
           value={this.state.value}
           onChange={this.handleInputChange.bind(this)}
           placeholder="Job Title"
         />
-         <Button id='search-button' className="btn hidden-sm-down">
-          <span className="glyphicon glyphicon-search"> </span>
+         <Button id='search-button' className="btn btn-default hidden-sm-down">
+          <span className="glyphicon glyphicon-search"></span>
          </Button>
       </div>
     );
